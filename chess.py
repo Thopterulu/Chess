b64787ff24ebb8ee8c62d880ebabaa631a85743a
Guillaume Kergueris <guillaume.kergueris@gmail.com>
2021-05-13 20:39:53 +0200

    trailing spaces

--- chess.py
+++ chess.py
@@ -2,16 +2,16 @@
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
- 
+
 class Window(QMainWindow):
    def __init__(self):
        super().__init__()
- 
+
        self.setGeometry(300, 300, 800, 600)
        self.setWindowTitle("Chess")
        self.show()
        self.activateWindow()
- 
+
 app = QApplication(sys.argv)
 window = Window()
 sys.exit(app.exec_())
